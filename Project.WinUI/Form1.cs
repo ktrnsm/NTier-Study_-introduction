f111114b7067d74e2eaf0e6981f6fba75a909ca7
iktueren <iktuerensemih@gmail.com>
2022-12-22 01:08:58 +0300

    WinUI added

--- Project.WinUI/Form1.cs
+++ Project.WinUI/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using Project.BLL.DesignPatterns.GenericRepostory.ConcRep;
+using Project.ENTITIES.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,16 +15,63 @@ namespace Project.WinUI
     public partial class Form1 : Form
     {
         CategoryRepository _cRep;
+        ProductRepository _pRep;
 
         public Form1()
         {
             InitializeComponent();
+            _cRep = new CategoryRepository();
+            _pRep = new ProductRepository();
         }
 
         private void btnCategories_Click(object sender, EventArgs e)
         {
-            listBox1.DataSource=_cRep.Get
+            BringCategories();
 
+        }
+
+        private void BringCategories()
+        {
+            listBox1.DataSource = _cRep.GetActives();
+            listBox1.SelectedIndex = -1;
+        }
+        private void BringPassives()
+        {
+            listBox1.DataSource = _cRep.GetPassives();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _cRep.Add(new ENTITIES.Models.Category
+            {
+                CategoryName = textBox1.Text
+            });
+            BringCategories();
+        }
+
+        Category _selected;
+
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+            if(listBox1.SelectedItem!=null)
+            {
+                _selected = listBox1.SelectedItem as Category;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(_selected!=null)
+            {
+                _cRep.Delete(_selected);
+                _selected = null;
+            }
+            BringCategories();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BringPassives();
         }
     }
 }
